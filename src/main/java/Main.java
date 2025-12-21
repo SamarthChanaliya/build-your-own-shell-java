2448499a3a902f9c01af1f93377cd5ca7062af90
Samarth Chanaliya <65223021+SamarthChanaliya@users.noreply.github.com>
2025-12-21 17:27:04 +0530

    echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,36 @@
+//Code Crafters
+//Build your own shell
+// pregress - 4/42
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner userInput = new Scanner(System.in);
-        
+
+        final String EXIT = "exit"; 
+        final String ECHO = "echo";
+
 
         while (true) {
             System.out.print("$ ");
-            String command = userInput.nextLine();
-            if (command.equalsIgnoreCase("exit")) {
+            String prompt = userInput.nextLine();
+
+
+            if (prompt.equalsIgnoreCase(EXIT)) {
                 break;
             }
-            System.out.println(command + ": command not found");
+            else if (prompt.startsWith(ECHO)) {
+                if (prompt.length() <= 4) {
+                    System.out.println();
+                }
+                else{
+                    System.out.println(prompt.substring(ECHO.length() + 1));
+                }
+            }
+            else{
+                System.out.println(prompt + ": command not found");
+            }
         }
         userInput.close();
     }
