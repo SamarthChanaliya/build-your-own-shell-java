c2131ea48653d1c5041bae2104d9b35fb0064a37
Samarth Chanaliya <65223021+SamarthChanaliya@users.noreply.github.com>
2025-12-27 12:00:56 +0530

    Search executables with type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,14 @@
 // Build your own shell
 // progress - 4/42
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -15,6 +21,9 @@ public class Main {
         builtinCommands.add("echo");
         builtinCommands.add("type");
 
+        String path = System.getenv("PATH");
+        String[] dirs = path.split(File.pathSeparator);
+
         boolean running = true;
 
         while (running) {
@@ -23,6 +32,7 @@ public class Main {
             String arguments = "";
 
             boolean commandFound = false;
+            
 
             
             for (String command : builtinCommands) {
@@ -53,8 +63,11 @@ public class Main {
                     }
 
                     else if (command.equals("type")) {
+                        Path foundPath = findInPath(arguments, dirs);
                         if (builtinCommands.contains(arguments)) {
                             System.out.println(arguments + " is a shell builtin");
+                        } else if (foundPath != null) {
+                            System.out.println(arguments + " is " + foundPath);
                         } else {
                             System.out.println(arguments + ": not found");
                         }
@@ -68,5 +81,16 @@ public class Main {
         }
 
         userInput.close();
+    }
+
+    // use Files.walk to find the command name in the array of directories
+    public static Path findInPath(String commandName, String[] directories) throws IOException{
+        for (String directory : directories) {
+            Path path = Path.of(directory);
+            if (Files.exists(path) || Files.isExecutable(path)) {
+                return path;
+            }
+        }
+        return null;
     }
 }
