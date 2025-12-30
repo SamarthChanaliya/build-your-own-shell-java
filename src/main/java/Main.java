2cf864e6319d96a44188c0c39cfbc0373fbfebdc
Samarth Chanaliya <65223021+SamarthChanaliya@users.noreply.github.com>
2025-12-30 23:58:58 +0530

    Implement tokenization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
