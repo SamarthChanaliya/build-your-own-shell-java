bbb2fa43cdc7e993c5f7539b771f7e893ae10d90
Samarth Chanaliya <65223021+SamarthChanaliya@users.noreply.github.com>
2026-01-02 01:07:54 +0530

    removed never thrown exception from main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
         Scanner userInput = new Scanner(System.in);
 
