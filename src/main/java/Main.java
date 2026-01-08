73884aa14574a08a718969a58fb458839eb4fca7
Samarth Chanaliya <65223021+SamarthChanaliya@users.noreply.github.com>
2026-01-08 13:02:44 +0530

    Implement Double Quotes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
 
@@ -37,15 +37,15 @@ public class Main {
             List<String> arguments = List.of();
             String command;
 
-            if (!tokens.isEmpty()){
+            if (!tokens.isEmpty()) {
                 command = tokens.getFirst();
-            }else continue;
+            } else continue;
 
 
-            if (command.length() > 1){
+            if (command.length() > 1) {
                 arguments = tokens.subList(1, tokens.size());
             }
-            String parsedArgs = String.join(" ",arguments);
+            String parsedArgs = String.join(" ", arguments);
 
 
             label:
@@ -96,7 +96,7 @@ public class Main {
         userInput.close();
     }
 
-    public static Path findInPath(String commandName, String[] directories){
+    public static Path findInPath(String commandName, String[] directories) {
         for (String directory : directories) {
             Path path = Path.of(directory, commandName);
             if (Files.exists(path) && Files.isExecutable(path)) {
@@ -120,32 +120,42 @@ public class Main {
     private static ArrayList<String> parseTokens(String input) {
         final char WHITE_SPACE = ' ';
         final char SINGLE_QUOTE = '\'';
-        boolean insideQuotes = false;
+        final char DOUBLE_QUOTE = '\"';
+        boolean insideSingleQuotes = false;
+        boolean insideDoubleQuotes = false;
 
-        StringBuilder stringBuilder = new StringBuilder();
-        ArrayList<String> tokens = new ArrayList<>();
+        StringBuilder charBuffer = new StringBuilder();
+        ArrayList<String> tokenList = new ArrayList<>();
 
         for (int i = 0; i < input.length(); i++) {
             char inputCharacter = input.charAt(i);
-            if (inputCharacter == SINGLE_QUOTE && !insideQuotes){
-                insideQuotes = true;
+
+            if (inputCharacter == SINGLE_QUOTE && !insideSingleQuotes && !insideDoubleQuotes) {
+                insideSingleQuotes = true;
                 continue;
-            } else if (inputCharacter == SINGLE_QUOTE) {
-                insideQuotes = false;
+            } else if (inputCharacter == SINGLE_QUOTE && !insideDoubleQuotes) {
+                insideSingleQuotes = false;
                 continue;
             }
-            if (inputCharacter != WHITE_SPACE && !insideQuotes) {
-                stringBuilder.append(inputCharacter);
-            } else if (inputCharacter == WHITE_SPACE && !stringBuilder.isEmpty() && !insideQuotes ) {
-                tokens.add(stringBuilder.toString());
-                stringBuilder.setLength(0);
-            } else if (inputCharacter != WHITE_SPACE) {
-                stringBuilder.append(inputCharacter);
+            if (inputCharacter == DOUBLE_QUOTE && !insideDoubleQuotes && !insideSingleQuotes) {
+                insideDoubleQuotes = true;
+                continue;
+            } else if (inputCharacter == DOUBLE_QUOTE && !insideSingleQuotes) {
+                insideDoubleQuotes = false;
+                continue;
+            }
+            if (inputCharacter != WHITE_SPACE && !insideSingleQuotes) {
+                charBuffer.append(inputCharacter);
+            } else if (inputCharacter == WHITE_SPACE && !charBuffer.isEmpty() && !insideSingleQuotes) {
+                tokenList.add(charBuffer.toString());
+                charBuffer.setLength(0);
+            } else if (insideSingleQuotes) {
+                charBuffer.append(inputCharacter);
             }
         }
-        if (!stringBuilder.isEmpty()) {
-            tokens.add(stringBuilder.toString());
+        if (!charBuffer.isEmpty()) {
+            tokenList.add(charBuffer.toString());
         }
-        return tokens;
+        return tokenList;
     }
 }
